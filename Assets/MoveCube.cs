f932328c2f8db1914d83b1babbde883c87a98f99
Seth T <seth.thomason@siu.edu>
2022-11-05 19:53:56 -0500

    game is now more realistic

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -6,13 +6,13 @@ public class MoveCube : MonoBehaviour
 {
 
     // Update is called once per frame
+
     void Update()
     {
         float xDirection = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-
+        Vector3 moveDirection = new Vector3(.3f*xDirection, 0.0f, .3f*zDirection);
         transform.position += moveDirection;
     }
 }
