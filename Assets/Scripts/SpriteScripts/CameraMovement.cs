115b3ee44cd79c3d395afca070462b8cf6c7a5a3
Jacob Bowers <jacob.bowers@siu.edu>
2022-11-29 19:11:50 -0600

    Added Purple Mist / Camera Improvements / Sprite Work

--- Assets/Scripts/SpriteScripts/CameraMovement.cs
+++ Assets/Scripts/SpriteScripts/CameraMovement.cs
@@ -4,36 +4,49 @@ using UnityEngine;
 
 
 
-public class CameraMovement : MonoBehaviour
+public class camera_movement : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform target;
-    [SerializeField] private float distanceToTarget = 10;
+    [SerializeField] private float distance_to_target = 10;
     
-    private Vector3 previousPosition;
+    private Vector3 previous_position;
     
+    void Start() 
+    {
+       update_position();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            previous_position = cam.ScreenToViewportPoint(Input.mousePosition);
         }
         else if (Input.GetMouseButton(0))
         {
-            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
-            Vector3 direction = previousPosition - newPosition;
-            
-            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
-            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
-            
-            cam.transform.position = target.position;
-            
-            //cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
-            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
-            
-            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
-            
-            previousPosition = newPosition;
+            update_position();
         }   
     }
+
+
+     private void update_position()
+     {
+        Vector3 new_position = cam.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 direction = previous_position - new_position;
+            
+        float rotation_around_y_axis = -direction.x * 180; // camera moves horizontally
+        // float rotation_around_x_axis = direction.y * 180; // camera moves vertically
+            
+        cam.transform.position = target.position;
+            
+        //cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+        cam.transform.Rotate(new Vector3(0, 1, 0), rotation_around_y_axis, Space.World); // <— This is what makes it work!
+            
+        int buffer = 26; // Pushes Camera up by 10.
+
+        cam.transform.Translate(new Vector3(0, buffer, -distance_to_target));
+            
+        previous_position = new_position;
+     }
 }
